538db84d9909132f9b9636c7092001330e93d0fb
ashikMostofaTonmoy <tonmoy.ashik@gmail.com>
2022-12-14 14:08:02 +0600

    added comments

--- batchInf.py
+++ batchInf.py
@@ -18,46 +18,47 @@ def get_cattle_task(self, side_path, rear_path):
     return data
 
 
-# %%
-path = 'data/'
-files = fnr.filname_ret(rootpath=path, file_types=('.jpg')).fileDirectory
-dfm = pd.read_csv(
-    'data/output tables/animals_management_weightsession.csv', index_col=0)
-# dfm = pd.read_csv('data/output tables/animals_management_weightsession.csv')
-dfv = pd.read_excel(
-    'data/output tables/221130_bhalo_Acme AI_Oct 2022_sanitized.xlsx', index_col='ID Number (App)')
-# %%
-dfside = dfm.side_img.str.split(pat='/', expand=True)[2]
-dfrear = dfm.rear_img.str.split(pat='/', expand=True)[2]
+dataPath = 'data/'
+mPowerCsvPath = 'data/output tables/animals_management_weightsession.csv'
+valoSanitizedExcelPath = 'data/output tables/221130_bhalo_Acme AI_Oct 2022_sanitized.xlsx'
+csvDestination = 'data/output tables'
+csv_name = 'valoAndMpowerMergedWithID.csv'
 
 # %%
-dftf = pd.DataFrame(files, columns=['fileWithDirectory'])
-# %%
-dfm['sideImgNameOnly'] = dfm.side_img.str.split(pat='/', expand=True)[2]
-# %%
-dfm['rearImgNameOnly'] = dfm.rear_img.str.split(pat='/', expand=True)[2]
-# %%
-# dftf = dftf.set_index('fileWithDirectory')
-# %%
-dftf['NameOnly'] = dftf.fileWithDirectory.str.split(pat='/', expand=True)[2]
-
-# %%
-df_mv = pd.merge(dfm, dfv, how='right', left_on='id',
-                 right_on='ID Number (App)')
-
-# %%
-dftf_side = dftf[dftf.NameOnly.isin(df_mv.sideImgNameOnly)]
-df_mv['sideImgLoc'] = dftf_side.fileWithDirectory.values
-
-# %%
-dftf_rear = dftf[dftf.NameOnly.isin(df_mv.rearImgNameOnly)]
-
-df_mv['rearImgLoc'] = dftf_rear.fileWithDirectory.values
-
-# %%
-destination = 'data/output tables'
-savefile_name = 'valoAndMpowerMergedWithID.csv'
-df_mv.to_csv(os.path.join(destination, savefile_name))
 
 
+def mergedCSV(imgDataPath=dataPath, mpCsvPath=mPowerCsvPath, vExlPath=valoSanitizedExcelPath, outPath=csvDestination, outFname=csv_name):
+    # raed imagaes from directory and list
+    files = fnr.filname_ret(rootpath=imgDataPath,
+                            file_types=('.jpg')).fileDirectory
+
+    # read excel & csv files and convert all data to data frames
+    dfm = pd.read_csv(mpCsvPath, index_col=0)
+    dfv = pd.read_excel(vExlPath, index_col='ID Number (App)')
+    dftf = pd.DataFrame(files, columns=['fileWithDirectory'])
+
+    # extract names and expand dataframe
+    dfm['sideImgNameOnly'] = dfm.side_img.str.split(pat='/', expand=True)[2]
+    dfm['rearImgNameOnly'] = dfm.rear_img.str.split(pat='/', expand=True)[2]
+    dftf['NameOnly'] = dftf.fileWithDirectory.str.split(
+        pat='/', expand=True)[2]
+
+    # merge valo and mpower dataframe
+    df_mv = pd.merge(dfm, dfv, how='right', left_on='id',
+                     right_on='ID Number (App)')
+
+    # filter image path  from dataframe
+    dftf_side = dftf[dftf.NameOnly.isin(df_mv.sideImgNameOnly)]
+
+    # copy image file path to dataframe
+    df_mv['sideImgLoc'] = dftf_side.fileWithDirectory.values
+    dftf_rear = dftf[dftf.NameOnly.isin(df_mv.rearImgNameOnly)]
+    df_mv['rearImgLoc'] = dftf_rear.fileWithDirectory.values
+
+    # export to CSV
+    df_mv.to_csv(os.path.join(outPath, outFname))
+
+
 # %%
+mergedCSV(imgDataPath=dataPath, mpCsvPath=mPowerCsvPath,
+          vExlPath=valoSanitizedExcelPath, outPath=csvDestination, outFname=csv_name)
